635be0fb6b78e774fd1cdcfe3bb7dd8497d14744
Tiago Seben <tiagolbs31@gmail.com>
2023-07-15 00:57:13 -0300

    Add message-create event

---
+++ events/message-create.js
@@ -0,0 +1,23 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.MessageCreate,
+	async execute(message, client) {
+		if (message.author.bot) {
+			return;
+		}
+
+		if (message.content.includes('@here') || message.content.includes('@everyone')) {
+			return;
+		}
+
+		const mentions = message.mentions.users;
+		if (mentions.has(client.user.id)) {
+			const messageContent = message.content.replace(/<.*?>/g, '').trim();
+
+			message.channel.sendTyping();
+
+			message.channel.send('teste');
+		}
+	},
+};
